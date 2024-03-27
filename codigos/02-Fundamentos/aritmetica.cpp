caf71f97bfaa67844f5e3d7d4bedac75796c20ce
Wesin Alves <wesinec@gmail.com>
2024-03-26 23:56:18 -0300

    add new files

---
+++ codigos/02-Fundamentos/aritmetica.cpp
@@ -0,0 +1,14 @@
+/*
+Testando aritmética em c++
+Autor: Wesin Ribeiro
+Data: 26/03/2024
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    x = 4 + 6;
+    cout << x / 5 << ' ' << x * 2 << endl;
+    return 0;
+}
---
+++ codigos/02-Fundamentos/hello.cpp
@@ -0,0 +1,14 @@
+/*
+Primeiro programa em c++
+Autor: Wesin Ribeiro
+Data: 26/03/2024
+*/
+#include <iostream>
+using namespace std;
+
+// Inicio da função principal
+int main(){
+    cout << "Hello, world!\n"; // imprime no terminal
+    
+    return 0; // indica que o programa finalizou corretamente
+}
